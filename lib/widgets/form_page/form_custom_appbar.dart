225bc27e7a4947aecae3b55ea359f9d612c57632
unknown <jesusmedinagajardo@gmail.com>
2025-01-10 04:50:22 -0300

    first commit

--- lib/widgets/form_page/form_custom_appbar.dart
+++ lib/widgets/form_page/form_custom_appbar.dart
@@ -1,36 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:prueba_final_flutter/constants/app_colors.dart';
 
 class FormCustomAppbar extends StatelessWidget {
   const FormCustomAppbar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: FractionallySizedBox(
-        alignment: Alignment(0, 1),
-        heightFactor: 0.45,
-        widthFactor: 0.95,
-        child: Row(
-          children: [
-            IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () => Navigator.pop(context),
-            ),
-            Expanded(
-              child: Align(
-                alignment: Alignment(-0.2, 0),
-                child: Text(
-                  "Form To Add Book",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                  ),
+    return FractionallySizedBox(
+      alignment: Alignment(0, 1),
+      heightFactor: 0.45,
+      widthFactor: 0.95,
+      child: Row(
+        children: [
+          IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () => Navigator.pop(context),
+          ),
+          Expanded(
+            child: Align(
+              alignment: Alignment(-0.2, 0),
+              child: Text(
+                "Form To Add Book",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
